e2c1eba6e51606cd8d5f16f3aabbde633a1bf53d
SylvaDev <irvingcsylva@gmail.com>
2025-09-14 20:01:20 -0700

    data migration up function fleshed out

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,19 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments()
+
+    table.string('vin', 17).notNullable().unique()
+
+    table.string('make', 128).notNullable()
+
+    table.string('model', 128).notNullable()
+
+    table.numeric('mileage').unsigned().notNullable()
+
+    table.string('title', 128).defaultTo('Unknown')
+
+    table.string('transmission', 128).defaultTo('Unknown')
+  })
 };
 
 exports.down = function (knex) {
