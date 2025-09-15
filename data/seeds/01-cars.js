f9b974de3981134e89a653e3e2e5aab2fc8eb4c3
SylvaDev <irvingcsylva@gmail.com>
2025-09-14 19:48:38 -0700

    added cars to the cars seed and scripts for seeding and migrating db

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,43 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'camry',
+        mileage: 125045,
+        title: 'clear',
+        transmission: 'automatic'
+    },
+    {
+        vin: '2222222222222',
+        make: 'nissan',
+        model: 'altima',
+        mileage: 77000,
+        title: 'clear',
+        transmission: 'manual'
+    },
+    {
+        vin: '1212121212123',
+        make: 'honda',
+        model: 'civic',
+        mileage: 245000,
+        title: 'salvaged',
+    },    {
+        vin: '3232451245874',
+        make: 'ford',
+        model: 'raptor',
+        mileage: 12045,
+    },    {
+        vin: '6589457852145',
+        make: 'dodge',
+        model: 'caravan',
+        mileage: 5044,
+        title: 'clear',
+        transmission: 'automatic'
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
