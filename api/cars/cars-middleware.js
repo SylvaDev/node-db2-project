cc1a1a7162f3d0a134a7a778a65989a5d5e3532e
SylvaDev <irvingcsylva@gmail.com>
2025-09-14 21:14:27 -0700

    fleshed out all of the cars-model functions, added middleware for checking id when getting cars by id

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,34 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Car = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car =  await Car.getById(req.params.id)
+    if (!car) {
+      next({ status: 404, message: 'no car with that id' })
+    } else {
+      req.car = car
+      next()
+    }
+} catch(err) {
+    next(err)
+}
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  next({ status: 404, message: 'payload incorrect'})
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  next({ status: 404, message: 'vin number not valid'})
 }
 
 const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  next({ status: 404, message: 'vin not unique'})
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
 }
